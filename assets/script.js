051bd25e94314a9c082f68a3fa5990c0e9b875b8
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-09-28 22:39:41 -0700

    Erased military time array, implemented the timeDisplayed array to the classes. Added code for column and was successfully able to display time on the column side.

--- assets/script.js
+++ assets/script.js
@@ -5,12 +5,13 @@ setInterval(function () {
 $(document).ready(function () {
     const timeDisplayed = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
-    const times = ["9am", "10am", "11am", "12pm", "13pm", "14pm", "15pm", "16pm", "17pm"];
-
     const row = $("<div>");
     row.addClass("row time-block");
-    row.attr("data-time", times[0]);
+    row.attr("data-time", timeDisplayed[0]);
     $(".container").append(row);
 
-    row.append();
+    const column = $("<div>");
+    column.addClass("col-1 hour");
+    column.text(timeDisplayed[0]);
+    row.append(column);
 })
