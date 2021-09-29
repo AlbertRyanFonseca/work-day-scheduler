a50cfaff5291615c30b75b8c481f7bda676680cc
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-09-28 22:32:37 -0700

    Able to append a row in the container div

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,16 @@
-var today = moment().format("MMMM Do, YYYY - hh:mm:ss a");
-console.log(today);
+setInterval(function () {
+    $(currentDay).text(moment().format('MMMM Do, YYYY h:mm:ss a'));
+}, 1000)
 
-var displayToday = function () {
-    $("#currentDay").text(today);
-}
+$(document).ready(function () {
+    const timeDisplayed = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
-displayToday();
+    const times = ["9am", "10am", "11am", "12pm", "13pm", "14pm", "15pm", "16pm", "17pm"];
+
+    const row = $("<div>");
+    row.addClass("row time-block");
+    row.attr("data-time", times[0]);
+    $(".container").append(row);
+
+    row.append();
+})
