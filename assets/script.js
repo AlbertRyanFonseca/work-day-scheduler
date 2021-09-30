07a476a1d0a2ce485703f668a888acf1b46b49ca
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-09-29 19:50:54 -0700

    Added for loop, insterted i instead of the 0. Fixed parameter of line 24. Accidentally typed times instead of timeDisplayed.

--- assets/script.js
+++ assets/script.js
@@ -5,31 +5,33 @@ setInterval(function () {
 $(document).ready(function () {
     const timeDisplayed = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
-    const row = $("<div>");
-    row.addClass("row time-block");
-    row.attr("data-time", timeDisplayed[0]);
-    $(".container").append(row);
+    for (let i = 0; i < timeDisplayed.length; i++) {
+        const row = $("<div>");
+        row.addClass("row time-block");
+        row.attr("data-time", timeDisplayed[i]);
+        $(".container").append(row);
 
-    const timeColumn = $("<div>");
-    timeColumn.addClass("col-1 hour");
-    timeColumn.text(timeDisplayed[0]);
-    row.append(timeColumn);
+        const timeColumn = $("<div>");
+        timeColumn.addClass("col-1 hour");
+        timeColumn.text(timeDisplayed[i]);
+        row.append(timeColumn);
 
-    const userTextArea = $("<textarea>");
-    userTextArea.addClass("col-10 description");
-    userTextArea.attr("id", timeDisplayed[0]);
-    row.append(userTextArea);
+        const userTextArea = $("<textarea>");
+        userTextArea.addClass("col-10 description");
+        userTextArea.attr("id", timeDisplayed[i]);
+        row.append(userTextArea);
 
-    var timeArrayVal = localStorage.getItem(times[i]);
-    if (timeArrayVal != null) {
-        userTextArea.val(timeArrayVal);
+        var timeArrayVal = localStorage.getItem(timeDisplayed[i]);
+        if (timeArrayVal != null) {
+            userTextArea.val(timeArrayVal);
+        }
+
+        const saveButton = $("<button>");
+        saveButton.addClass("col-1 saveBtn");
+        const saveIcon = $("<i class='far fa-save fa-lg'></i>");
+        saveButton.append(saveIcon);
+        row.append(saveButton);
     }
-
-    const saveButton = $("<button>");
-    saveButton.addClass("col-1 saveBtn");
-    const saveIcon = $("<i class='far fa-save fa-lg'></i>");
-    saveButton.append(saveIcon);
-    row.append(saveButton);
 
     $(".saveBtn").on("click", function (event) {
         event.preventDefault();
